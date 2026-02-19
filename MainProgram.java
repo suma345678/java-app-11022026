fb769a60b008939ffc65b1739757c1b1119c0edc
suma345678 <164640883+suma345678@users.noreply.github.com>
2026-02-19 20:53:17 +0530

    Update employee details in MainProgram.java

--- MainProgram.java
+++ MainProgram.java
@@ -4,30 +4,30 @@ import com.codegnan.Day17.module1.Employee;
 
 public class MainProgram {
 	public static void main(String[] args) {
-		//Employee employee = new Employee(101, "Akshat", "Kumar", "31/12/2025", "akshat@gmail.com", "9988776655");
+		//Employee employee = new Employee(101, "Shashi", "suma", "31/1/2026", "shashisuma493@gmail.com", "9985568971");
 		
 		/*Employee employee = new Employee();
 		employee.setEmployeeId(101);
-		employee.setFirstName("Akshat");
-		employee.setLastName("Kumar");
-		employee.setDateOfJoining("31/12/2025");
-		employee.setEmailAddress("akshat@gmail.com");
-		employee.setMobileNumber("9988776655");*/
+		employee.setFirstName("Shashi");
+		employee.setLastName("suma");
+		employee.setDateOfJoining("31/1/2026");
+		employee.setEmailAddress(shashisuma493@gmail.com");
+		employee.setMobileNumber("9985568971");*/
 		
-		/*Employee employee1 = new Employee(101, "Akshat", "Kumar", "31/12/2025", "akshat@gmail.com", "9988776655");
-		Employee employee2 = new Employee(102, "Chetan", "Sharma");
-		Employee employee3 = new Employee("Neha", "Gupta");
+		/*Employee employee1 = new Employee(101, "Shashi", "suma", "31/1/2026", "shashisuma493@gmail.com", "9985568971");
+		Employee employee2 = new Employee(102, "pooja", "Sharma");
+		Employee employee3 = new Employee("pragati", "reddy");
 		Employee employee4 = new Employee();*/
 		
 		/*Employee employee = new Employee();
 		employee.setEmployeeId(101);
-		employee.setFirstName("Akshat");
-		employee.setLastName("Kumar");
-		employee.setDateOfJoining("31/12/2025");
-		employee.setEmailAddress("akshat@gmail.com");
-		employee.setMobileNumber("9988776655");*/
+		employee.setFirstName("Shashi");
+		employee.setLastName("suma");
+		employee.setDateOfJoining("31/1/2026");
+		employee.setEmailAddress("shashisuma493@gmail.com");
+		employee.setMobileNumber("9985568971");*/
 		
-		Employee employee = new Employee(101, "Akshat", "Kumar", "31/12/2025", "akshat@gmail.com", "9988776655");
+		Employee employee = new Employee(101, "Shashi", "suma", "31/1/2026", "shashisuma493@gmail.com", 9985568971");
 		
 		int empId = employee.getEmployeeId();
 		System.out.println("Employee ID: " + empId);
